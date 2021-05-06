a8998493322669620aca9655a1cd3d7b0cd7accb
vtiaviz <victor.ti@bol.com.br>
2021-05-05 21:22:27 -0300

    Exercicio GIt Victor

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const meuArray = [];
 let contador = 0;
+let num = 0;
 
 while (contador <= 100) {
-	meuArray.push(contador);
+	if (contador % 2 !== 0 ) {
+		num = num + contador;
+	}
 	contador++;
 }
-
+meuArray.push(num);	
+alert(meuArray)
 // Fazer uma função que soma todos os valores ímpares do array e retorna em um popup (alert)
