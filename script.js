adbacbd2bd134844638776585502a5b7491407d8
Charles Godoy <charles1218.cg@gmail.com>
2021-05-07 18:23:00 -0300

    subindo desafio

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ while (contador <= 100) {
 }
 
 // Fazer uma função que soma todos os valores ímpares do array e retorna em um popup (alert)
+let soma=0;
+	for (let numImpar=1; numImpar<=100;numImpar++){
+		if(numImpar %2!=0){
+			/*console.log(numImpar);*/
+			soma= soma+numImpar;
+	}
+}
+ alert(" A Soma dos números Impares de 0 à 100 é :"+soma);
+ //minha atualizaçao agora vai...
