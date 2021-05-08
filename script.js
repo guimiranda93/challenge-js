dcdf6c2f03470ec33137981aca01a2cd41c01906
Fabiana <fabi.arieira@gmail.com>
2021-05-07 21:22:24 -0300

    exercicio fabiana

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const meuArray = [];
 let contador = 0;
+let n = 0;
 
 while (contador <= 100) {
-	meuArray.push(contador);
-	contador++;
+    if (contador % 2 !== 0) { //verifica se o numero é primo
+        n = n + contador; //se for primo vai somar
+    }
+    contador++;
 }
+meuArray.push(n);
+alert(meuArray)
 
 // Fazer uma função que soma todos os valores ímpares do array e retorna em um popup (alert)
